2fb19bdb2568436e61e551bd1518f11ceb2d4ba5
theSherwood <adam.the.sherwood@gmail.com>
2020-06-28 03:12:05 -0600

    Add test api to nginx config.

--- api/index.js
+++ api/index.js
@@ -7,6 +7,6 @@ app.get('/api/', function (req, res) {
 	res.json({ testResponse: [process.env.TEST_SECRET, process.env.NODE_ENV] });
 });
 
-app.listen(5000, function () {
-	console.log('Express server listening on port 5000.');
+app.listen(5001, function () {
+	console.log('Express server listening on port 5001.');
 });
